ee883285e9bd7ab1fc005074b13508969c2ab5a2
Luciano Silveira <ladrians@gmail.com>
2021-08-25 08:21:32 -0300

    FP.3

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -19,9 +19,31 @@ template <class T>
 class MessageQueue
 {
 public:
+    T receive()
+    {
+        // perform queue modification under the lock
+        std::unique_lock<std::mutex> uLock(_mutex);
+        _cond.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
+        // remove last vector element from queue
+        T msg = std::move(_queue.back());
+        _queue.pop_back();
+        return msg; // will not be copied due to return value optimization (RVO) in C++
+    }
 
+    void send(T &&msg)
+    {
+        // simulate some work
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // perform vector modification under the lock
+        std::lock_guard<std::mutex> uLock(_mutex);
+        // add vector to queue
+        _queue.push_back(std::move(msg));
+        _cond.notify_one(); // notify client after pushing new Vehicle into vector
+    }
 private:
-    
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    std::deque<T> _queue;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
